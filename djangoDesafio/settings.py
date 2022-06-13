2041aa510addeef1421ddd7212c861714f0b6be2
Leandro Arruda <leandro.arruda@inteli.edu.br>
2022-06-12 23:51:53 -0300

    fix: :bug: added vercel in allowed origins

--- djangoDesafio/settings.py
+++ djangoDesafio/settings.py
@@ -58,9 +58,11 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://desafio-front-end-three.vercel.app/',
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:3000',
+    'https://desafio-front-end-three.vercel.app/',
 ]
 
 ROOT_URLCONF = 'djangoDesafio.urls'
